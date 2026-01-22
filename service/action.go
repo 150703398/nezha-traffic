3cfc48109786e4e0b00765392ac732afbfcfcf89
150703398 <150703398@qq.com>
2026-01-22 15:46:39 +0800

    Update action.go

--- service/action.go
+++ service/action.go
@@ -1,23 +1,8 @@
 package service
 
-import (
-	"fmt"
-	"os/exec"
+import "github.com/你的用户名/nezha-traffic/model"
 
-	"github.com/nezhahq/nezha/model"
-)
-
-// ExecuteAutoAction 执行超限动作
+// ExecuteAutoAction 已取消，超限不做操作
 func ExecuteAutoAction(server model.Server) {
-	if server.TrafficPlan == nil {
-		return
-	}
-
-	switch server.TrafficPlan.AutoAction {
-
-	case "shutdown":
-		// ⚠️ 远程关机命令，需要服务器允许 SSH 密钥免密登录
-		cmd := fmt.Sprintf("ssh root@%s 'shutdown -h now'", server.Name)
-		_ = exec.Command("bash", "-c", cmd).Run()
-	}
+	return
 }
